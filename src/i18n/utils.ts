eb0e5e418cf72304bd1cce9ac5e6ad66b3d0fd85
Iñaki Mir <imirbelloso@gmail.com>
2026-02-03 18:12:15 +0100

    fix and finish all english translations

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -7,3 +7,43 @@ export function useTranslations(lang: keyof typeof labels) {
         return labels[lang][key] || labels[defaultLang][key];
     }
 }
+
+export const routes = {
+  reservas: {
+    es: "/reservas",
+    en: "/bookings",
+  },
+  cancelacion: {
+    es: "/cancelacion-y-devolucion",
+    en: "/cancellation-and-refund",
+  },
+  home: {
+    es: "/",
+    en: "/",
+  },
+} as const;
+
+
+export const getLocalizedRoute = (
+  key: keyof typeof routes,
+  lang: string
+) => {
+  const route = routes[key][lang as "es" | "en"] || routes[key].es;
+
+  if (lang === "es") return route;
+  return `/${lang}${route}`;
+};
+
+export function translatePath(path: string, targetLang: "es" | "en") {
+  // quitar prefijo idioma
+  const cleanPath = path.replace(/^\/(en)\//, "/");
+
+  for (const route of Object.values(routes)) {
+    if (route.es === cleanPath || route.en === cleanPath) {
+      return route[targetLang];
+    }
+  }
+
+  return cleanPath; // fallback
+}
+
